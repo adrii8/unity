a7b2693622b4b55c7b89643b8748ce62ff88f2c4
Adri Alcaraz <adrianalcarazgarcia@ied.edu>
2017-11-17 20:50:43 +0100

    actualizacion

---
+++ New Unity Project/Assets/scripts/BallControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControler : MonoBehaviour {
+
+	float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody> ();
+		this.speed = 1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontalAxis = Input.GetAxis("Horizontal");
+		float verticalAxis = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontalAxis, 0.0f,verticalAxis);
+		rb.AddForce (movement);
+	}
+}
